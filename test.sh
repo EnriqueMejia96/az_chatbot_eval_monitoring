16e8d685047d821a49d4d362fa2f6d746120c917
José Mejía <enriquemejiagamarra@gmail.com>
2025-09-08 11:03:31 -0500

    add: .sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+$RG       = "jemg-personal-test"
+$ACR  = "azcreu2labt01"
+$ACR_LOGIN_SERVER = az acr show -g $RG -n $ACR --query loginServer -o tsv
+$TAG = "latest"
+$IMAGE = "streamlit-chat-eval"
+$APP  = "wapceu2labt01"
+az acr build -r $ACR -t "${ACR_LOGIN_SERVER}/${IMAGE}:${TAG}" .
+
+az webapp config container set -g $RG -n $APP `
+  --container-image-name "${ACR_LOGIN_SERVER}/${IMAGE}:${TAG}" `
+  --container-registry-url "https://${ACR_LOGIN_SERVER}"
+az webapp restart -g $RG -n $APP
